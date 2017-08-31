e41062141e27e40e6800794c4d549faa9b61796c
Dominic Droz <dominicdroz1999@gmail.com>
2017-08-31 11:18:28 +0200

    ke ahnig was das isch
    hihi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("luc sucks small balls!");
+        System.out.println("luc sucks small balls! tehee");
     }
 }
